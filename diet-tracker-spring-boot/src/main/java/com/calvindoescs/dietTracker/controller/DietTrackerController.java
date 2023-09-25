8d61850a09f015a2e5c8d273133c3932d1c6b0cf
CalvinDoesCS <calvinntran.cs@gmail.com>
2023-09-25 11:00:39 -0400

    Change the request mapping

--- diet-tracker-spring-boot/src/main/java/com/calvindoescs/dietTracker/controller/DietTrackerController.java
+++ diet-tracker-spring-boot/src/main/java/com/calvindoescs/dietTracker/controller/DietTrackerController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping(value = "/api",produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 public class DietTrackerController {
     private UserServiceImpl userServiceImpl;
 
