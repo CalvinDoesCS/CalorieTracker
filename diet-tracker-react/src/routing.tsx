2500298a27310a11bacbf30d1f6919c91c7f6250
CalvinDoesCS <calvinntran.cs@gmail.com>
2023-10-10 16:58:46 -0400

    Added Routing to SignUp

--- diet-tracker-react/src/routing.tsx
+++ diet-tracker-react/src/routing.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "./pages/ErrorPage";
 import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
+import SignUpForm from "./components/SignUpForm";
 
 const router = createBrowserRouter([  
     {
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
         {
           index: true,
           element: <HomePage />,
+        },
+        {
+          path: "/signup",
+          element: <SignUpForm/>
         }
       ],
     },
