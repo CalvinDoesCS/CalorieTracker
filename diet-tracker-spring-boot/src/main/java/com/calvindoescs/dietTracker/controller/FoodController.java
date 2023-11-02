3cb7e554671cb59379ac507bf8242f0f76748383
CalvinDoesCS <calvinntran.cs@gmail.com>
2023-11-01 20:29:40 -0400

    Change Controller Name

--- diet-tracker-spring-boot/src/main/java/com/calvindoescs/dietTracker/controller/FoodController.java
+++ diet-tracker-spring-boot/src/main/java/com/calvindoescs/dietTracker/controller/FoodController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value="/api", produces = { "application/json" })
-public class DietTrackerController {
+public class FoodController {
 
 }
